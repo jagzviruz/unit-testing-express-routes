94ac3bbe57402c16736182a5e3a694591d547f00
Jagadish, K <k.jagdish@gmail.com>
2018-11-29 16:02:53 +0530

    unit test for endpoint

--- tests/users.tests.js
+++ tests/users.tests.js
@@ -1,11 +1,29 @@
-import { assert } from 'chai';
+import { assert, expect } from 'chai';
 import { before } from 'mocha';
+import request from 'supertest';
+import express from 'express';
+
+import usersRouter from '../src/api'
 
 describe(__filename, () => {
+  let app;
+
   before(() => {
-    console.log('bootstrap all tests in here');
+    app = express();
+
+    app.use('/users', usersRouter);
   })
-  it('test users endpoint', () => {
-    assert(true);
+  it('test users endpoint', (done) => {
+    request(app)
+      .get('/users/all')
+      .expect(200)
+      .end((err, { body }) => {
+        if (err) console.log(err);
+
+        assert(Array.isArray(body.users));
+        const { users } = body;
+        expect(users[0].name.first).to.equal('Delaney');
+        done();
+      });
   });
 })
